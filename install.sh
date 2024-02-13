8c2a81465a5215a05da83966b000122e2017f3a6
= <=>
2024-02-13 13:51:31 -0500

    adding nvim

--- install.sh
+++ install.sh
@@ -9,6 +9,9 @@ curl -sS https://starship.rs/install.sh | sh
 # Set as default shell
 chsh -s $(which zsh)
 
+#Install Neovim from package manager
+sudo apt install neovim
+
 #Clone repo and stow the config files???
 sudo apt install stow
 stow zsh
